6027888c2a34efa06b26e5af3c9f55afe80b4a8f
Mark Sands <marksands07@gmail.com>
2010-03-26 11:35:48 -0500

    added audio validation

--- functions/tour_audio.php
+++ functions/tour_audio.php
@@ -1,17 +1,41 @@
 <?php
 
-// TODO: validation!!!1!!!cos(0)!!
+	// define a constant for the maximum upload size, 500MB, fairly large
+define ('MAX_FILE_SIZE', 524288000); 
+
 function UploadMusic() {
 
+		// set handlers
+	$OK = false;
 	$path = WP_PLUGIN_DIR . '/wwt-creator/tours/' . $_FILES['audio-file']['name'];
 
-	 if ( $_FILES['audio-file']['type'] == "audio/mpeg" )
-		move_uploaded_file($_FILES['audio-file']['tmp_name'], $path);	
+		// valid file size
+	if ( $_FILES['file-upload']['size'] > 0 || $_FILES['file-upload']['size'] <= MAX_FILE_SIZE ) {
+		$OK = true;
+	}
+		
+		// valid MIME types
+	$permitted = array( 'audio/mpeg', 'audio/mpeg3', 'audio/x-mpeg-3', 
+		'audio/mpeg', 'audio/x-mpeg', 'audio/mp3',
+		'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3',
+		'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio',
+	);
+	
+ 		// check that file is of an permitted MIME type
+	foreach ($permitted as $type) {
+		if ( $type == $_FILES['file-upload']['type'] ) {
+			$OK = true;
+			break;
+		}
+	}
 
-	if ( $_FILES['audio-file']['name'] != NULL )
-		$webPath = WP_PLUGIN_URL.'/wwt-creator/tours/' . str_replace(" ", "%20", $_FILES['audio-file']['name'] );
-	else
-		$webPath = "";
+		// default empty path if not found
+	$webPath = "";
+		
+	if ( $OK && $_FILES['audio-file']['name'] != NULL ) {
+		move_uploaded_file($_FILES['audio-file']['tmp_name'], $path);	
+		$webPath = WP_PLUGIN_URL.'/wwt-creator/tours/' . str_replace(" ", "%20", $_FILES['audio-file']['name'] );		
+	}
 
 	return $webPath;
 }
